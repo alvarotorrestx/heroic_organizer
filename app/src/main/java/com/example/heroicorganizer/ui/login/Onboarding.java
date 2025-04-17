6c7f052cb024182ef060970f307d6abe4b3a26f6
Wendell Wilhelm <BlackWulf@student.fullsail.edu>
2025-04-17 17:07:03 -0400

    Commit: enhanced dashboard.

--- app/src/main/java/com/example/heroicorganizer/ui/login/Onboarding.java
+++ app/src/main/java/com/example/heroicorganizer/ui/login/Onboarding.java
@@ -2,11 +2,12 @@ package com.example.heroicorganizer.ui.login;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.heroicorganizer.R;
 
 public class Onboarding extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_onboarding);
+        setContentView(R.layout.activity_main);
     }
 }
