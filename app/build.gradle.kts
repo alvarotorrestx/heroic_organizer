fc674a64630fedbfc4a0f6ae54581973371a6021
Alvaro Torres <alvaro@alvarotorres.dev>
2025-05-12 22:58:18 -0500

    Added Material icons to project. Created a rounded background for camera icons. Created layout foundation for custom camera.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,9 @@ dependencies {
     implementation("androidx.camera:camera-lifecycle:1.3.0")
     implementation("androidx.camera:camera-view:1.3.0")
 
+    // Material Icons Dependency
+    implementation("com.google.android.material:material:1.11.0")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
