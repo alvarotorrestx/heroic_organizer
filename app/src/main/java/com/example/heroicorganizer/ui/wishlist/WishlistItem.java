0ad43e776100ab07d7e569ed2271597281f8e767
Sora G.D. <Purple-Sora@users.noreply.github.com>
2025-05-15 00:40:34 -0400

    Set up of wishlist logic and recycler view layout

---
+++ app/src/main/java/com/example/heroicorganizer/ui/wishlist/WishlistItem.java
@@ -0,0 +1,19 @@
+package com.example.heroicorganizer.ui.wishlist;
+
+public class WishlistItem {
+    public int imageResId;
+    public String title;
+    public String issueNum;
+    public String variant;
+    public String releaseDate;
+    public String cutoffDate;
+
+    public WishlistItem(int imageResId, String title, String issueNum, String variant, String releaseDate, String cutoffDate) {
+        this.imageResId = imageResId;
+        this.title = title;
+        this.issueNum = issueNum;
+        this.variant = variant;
+        this.releaseDate = releaseDate;
+        this.cutoffDate = cutoffDate;
+    }
+}
