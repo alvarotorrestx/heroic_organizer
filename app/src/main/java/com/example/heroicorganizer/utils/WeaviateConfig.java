26ad08a2b634c198ebdb31e75cda6b70d0f0182b
Alvaro Torres <atorr1020@gmail.com>
2025-05-19 22:35:40 -0500

    Increased scope of schema creation from Weaviate

--- app/src/main/java/com/example/heroicorganizer/utils/WeaviateConfig.java
+++ app/src/main/java/com/example/heroicorganizer/utils/WeaviateConfig.java
@@ -19,57 +19,124 @@ public class WeaviateConfig {
         // Use gson to create a clean json call
         Gson gson = new Gson();
 
-        Map<String, Object> schemaBody = new HashMap<>();
-        schemaBody.put("class", "Comic");
-        schemaBody.put("vectorizer", "img2vec-neural");
-        schemaBody.put("vectorIndexType", "hnsw");
+        // Comic class creation
+        Map<String, Object> comicSchema = new HashMap<>();
+        comicSchema.put("class", "Comic");
+        comicSchema.put("vectorizer", "none");
+        comicSchema.put("vectorIndexType", "hnsw");
 
-        Map<String, Object> moduleConfig = new HashMap<>();
-        moduleConfig.put("img2vec-neural", Map.of("imageFields", List.of("image")));
-        schemaBody.put("moduleConfig", moduleConfig);
-
-        List<Map<String, Object>> properties = new ArrayList<>();
-        properties.add(Map.of(
-                "name", "image",
-                "dataType", List.of("blob"),
-                "description", "The image to load"
-        ));
-        properties.add(Map.of(
+        // Set properties of Comic
+        List<Map<String, Object>> comicProperties = new ArrayList<>();
+        comicProperties.add(Map.of(
                 "name", "title",
                 "dataType", List.of("text"),
                 "description", "The comic title"
         ));
-        schemaBody.put("properties", properties);
+        comicProperties.add(Map.of(
+                "name", "publisherNames",
+                "dataType", List.of("text"),
+                "description", "Publisher names"
+        ));
+        comicProperties.add(Map.of(
+                "name", "comicVariants",
+                "dataType", List.of("ComicVariant"),
+                "description", "All variants of the comic"
+        ));
+        comicSchema.put("properties", comicProperties);
 
-        // Convert to JSON
-        String json = gson.toJson(schemaBody);
+        // ComicVariant class creation
+        Map<String, Object> comicVariantSchema = new HashMap<>();
+        comicVariantSchema.put("class", "ComicVariant");
+        comicVariantSchema.put("vectorizer", "img2vec-neural");
+        comicVariantSchema.put("vectorIndexType", "hnsw");
 
-        // Send a post request with the json body
-        RequestBody body = RequestBody.create(json, MediaType.parse("application/json"));
-        Request request = new Request.Builder()
-                .url("http://10.0.2.2:8080/v1/schema")
-                .post(body)
-                .addHeader("Content-Type", "application/json")
-                .build();
+        Map<String, Object> moduleConfig = new HashMap<>();
+        moduleConfig.put("img2vec-neural", Map.of("imageFields", List.of("image")));
+        comicVariantSchema.put("moduleConfig", moduleConfig);
 
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Log.e("WeaviateSchema", "Failed to create schema: " + e.getMessage());
-            }
+        // Properties that each Comic/Variant will hold - returned data from scanned comic
+        List<Map<String, Object>> comicMetadata = new ArrayList<>();
+        comicMetadata.add(Map.of(
+                "name", "image",
+                "dataType", List.of("blob"),
+                "description", "The image to load"
+        ));
+        comicMetadata.add(Map.of(
+                "name", "title",
+                "dataType", List.of("text"),
+                "description", "The comic title"
+        ));
+        comicMetadata.add(Map.of(
+                "name", "issueNumber",
+                "dataType", List.of("text"),
+                "description", "The issue number"
+        ));
+        comicMetadata.add(Map.of(
+                "name", "variantId",
+                "dataType", List.of("text"),
+                "description", "Variant ID"
+        ));
+        comicMetadata.add(Map.of(
+                "name", "parentComic",
+                "dataType", List.of("Comic"),
+                "description", "The main comic this variant belongs to"
+        ));
+        comicMetadata.add(Map.of(
+                "name", "coverArtist",
+                "dataType", List.of("text"),
+                "description", "Cover artist"
+        ));
+        comicMetadata.add(Map.of(
+                "name", "author",
+                "dataType", List.of("text"),
+                "description", "Comic author"
+        ));
+        comicMetadata.add(Map.of(
+                "name", "datePublished",
+                "dataType", List.of("text"),
+                "description", "Date of release"
+        ));
+        comicMetadata.add(Map.of(
+                "name", "upc",
+                "dataType", List.of("text"),
+                "description", "UPC code"
+        ));
+        comicMetadata.add(Map.of(
+                "name", "description",
+                "dataType", List.of("text"),
+                "description", "Comic description"
+        ));
+        comicVariantSchema.put("properties", comicMetadata);
 
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if (response.isSuccessful()) {
-                    Log.d("WeaviateSchema", "Schema successfully created!");
-                } else {
-                    Log.e("WeaviateSchema", "Schema creation failed: " + response.code() + " - " + response.message());
+        // Convert both schemas to JSON and send POST requests
+        List<Map<String, Object>> schemas = List.of(comicSchema, comicVariantSchema);
+
+        for (Map<String, Object> schema : schemas) {
+            String json = gson.toJson(schema);
+            RequestBody body = RequestBody.create(json, MediaType.parse("application/json"));
+            Request request = new Request.Builder()
+                    .url("http://10.0.2.2:8080/v1/schema")
+                    .post(body)
+                    .addHeader("Content-Type", "application/json")
+                    .build();
+
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    Log.e("WeaviateSchema", "Failed to create schema: " + e.getMessage());
                 }
 
-                // Helps avoid memory leaks
-                response.close();
-            }
-        });
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    if (response.isSuccessful()) {
+                        Log.d("WeaviateSchema", "Schema successfully created: " + schema.get("class"));
+                    } else {
+                        Log.e("WeaviateSchema", "Schema creation failed: " + response.code() + " - " + response.message());
+                    }
+                    response.close();
+                }
+            });
+        }
     }
 
     public static void checkWeaviateSchema() {
